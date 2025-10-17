00f4054401cb6036aacbdec7761aafe0e89894eb
MVuong1501 <148269388+MVuong1501@users.noreply.github.com>
2025-10-17 13:37:44 +0700

    khoi tao du an springboot

---
+++ src/main/java/com/doan/WEB_TMDT/WebTmdtApplication.java
@@ -0,0 +1,13 @@
+package com.doan.WEB_TMDT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTmdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTmdtApplication.class, args);
+	}
+
+}
