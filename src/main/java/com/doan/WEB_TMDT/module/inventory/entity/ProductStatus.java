d188e9b63f74c445b326149ad625d8cfb740d407
MVuong1501 <148269388+MVuong1501@users.noreply.github.com>
2025-11-06 09:47:56 +0700

    .

--- src/main/java/com/doan/WEB_TMDT/module/inventory/entity/ProductStatus.java
+++ src/main/java/com/doan/WEB_TMDT/module/inventory/entity/ProductStatus.java
@@ -6,9 +6,6 @@ public enum ProductStatus {
     SOLD,           // đã bán
     RETURNED,       // đã trả lại kho
     WARRANTY,       // đang gửi bảo hành
-<<<<<<< HEAD
-    SCRAPPED        // hư, không dùng được
-=======
     DAMAGED        // hư, không dùng được
->>>>>>> 01f2138d271c32ff6a3a47bc769f5dc7fc5fa154
+
 }
